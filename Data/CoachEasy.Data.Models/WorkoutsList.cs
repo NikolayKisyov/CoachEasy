00156a1a00b33aeeb6f4d064238bb4325f2887af
Nikolay Kisyov <niks_2000@abv.bg>
2020-12-05 00:49:43 +0200

    Uploading missing files

---
+++ Data/CoachEasy.Data.Models/WorkoutsList.cs
@@ -0,0 +1,24 @@
+﻿namespace CoachEasy.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    using CoachEasy.Data.Common.Models;
+
+    public class WorkoutsList : BaseDeletableModel<string>
+    {
+        public WorkoutsList()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public string WorkoutId { get; set; }
+
+        public virtual Workout Workout { get; set; }
+
+        public string ClientId { get; set; }
+
+        public virtual Client Client { get; set; }
+    }
+}
---
+++ Services/CoachEasy.Services.Data/Models/ClientDto.cs
@@ -0,0 +1,16 @@
+﻿namespace CoachEasy.Services.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    using CoachEasy.Data.Models;
+    using CoachEasy.Web.ViewModels;
+
+    public class ClientDto
+    {
+        public string Id { get; set; }
+
+        public ICollection<WorkoutsList> WorkoutsList { get; set; }
+    }
+}
